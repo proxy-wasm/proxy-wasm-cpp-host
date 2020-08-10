proxy-wasm/proxy-wasm-cpp-host

commit 56593833008e677a99b51a78cd480391f298e226

Support multiple versions of the Proxy-Wasm ABI. (#45)
Signed-off-by: Piotr Sikora <piotrsikora@google.com>

src/v8/v8.cc
@@ -54,6 +54,7 @@ public:
   string_view runtime() override { return "v8"; }
 
   bool load(const std::string &code, bool allow_precompiled) override;
+  AbiVersion getAbiVersion() override;
   string_view getCustomSection(string_view name) override;
   string_view getPrecompiledSectionName() override;
   bool link(string_view debug_name) override;
@@ -364,6 +365,24 @@ string_view V8::getPrecompiledSectionName() {
              std::to_string(V8_PATCH_LEVEL) + "_" + WEE8_PLATFORM)
           : "";
   return name;
+}
+
+AbiVersion V8::getAbiVersion() {
+  assert(module_ != nullptr);
+
+  const auto export_types = module_.get()->exports();
+  for (size_t i = 0; i < export_types.size(); i++) {
+    if (export_types[i]->type()->kind() == wasm::EXTERN_FUNC) {
+      std::string_view name(export_types[i]->name().get(), export_types[i]->name().size());
+      if (name == "proxy_abi_version_0_1_0") {
+        return AbiVersion::ProxyWasm_0_1_0;
+      } else if (name == "proxy_abi_version_0_2_0") {
+        return AbiVersion::ProxyWasm_0_2_0;
+      }
+    }
+  }
+
+  return AbiVersion::Unknown;
 }
 
 bool V8::link(string_view debug_name) {

include/proxy-wasm/context.h
@@ -235,6 +235,10 @@ public:
     t += tpe.tv_nsec;
     return t;
   }
+  string_view getConfiguration() override {
+    unimplemented();
+    return "";
+  }
   std::pair<uint32_t, string_view> getStatus() override {
     unimplemented();
     return std::make_pair(1, "unimplmemented");
@@ -312,6 +316,7 @@ public:
                                string_view /* details */) override {
     return unimplemented();
   }
+  void clearRouteCache() override { unimplemented(); }
   void failStream(WasmStreamType stream_type) override { closeStream(stream_type); }
 
   // Shared Data

include/proxy-wasm/context_interface.h
@@ -236,6 +236,9 @@ public:
                                        Pairs additional_headers, uint32_t grpc_status,
                                        string_view details) = 0;
 
+  // Clears the route cache for the current request.
+  virtual void clearRouteCache() = 0;
+
   // Call when the stream closes. See RootInterface.
   virtual bool onDone() = 0;
 
@@ -544,6 +547,9 @@ struct GeneralInterface {
 
   // Provides the current time in nanoseconds since the Unix epoch.
   virtual uint64_t getCurrentTimeNanoseconds() = 0;
+
+  // Returns plugin configuration.
+  virtual string_view getConfiguration() = 0;
 
   /**
    * Provides the status of the last call into the VM or out of the VM, similar to errno.

include/proxy-wasm/exports.h
@@ -29,17 +29,21 @@ namespace exports {
 
 // ABI functions exported from envoy to wasm.
 
+Word get_configuration(void *raw_context, Word address, Word size);
 Word get_status(void *raw_context, Word status_code, Word address, Word size);
 Word log(void *raw_context, Word level, Word address, Word size);
 Word get_property(void *raw_context, Word path_ptr, Word path_size, Word value_ptr_ptr,
                   Word value_size_ptr);
 Word set_property(void *raw_context, Word key_ptr, Word key_size, Word value_ptr, Word value_size);
+Word continue_request(void *raw_context);
+Word continue_response(void *raw_context);
 Word continue_stream(void *raw_context, Word stream_type);
 Word close_stream(void *raw_context, Word stream_type);
 Word send_local_response(void *raw_context, Word response_code, Word response_code_details_ptr,
                          Word response_code_details_size, Word body_ptr, Word body_size,
                          Word additional_response_header_pairs_ptr,
                          Word additional_response_header_pairs_size, Word grpc_status);
+Word clear_route_cache(void *raw_context);
 Word get_shared_data(void *raw_context, Word key_ptr, Word key_size, Word value_ptr_ptr,
                      Word value_size_ptr, Word cas_ptr);
 Word set_shared_data(void *raw_context, Word key_ptr, Word key_size, Word value_ptr,

include/proxy-wasm/null_plugin.h
@@ -39,6 +39,7 @@ namespace proxy_wasm {
  */
 struct NullPluginRegistry {
   void (*proxy_abi_version_0_1_0_)() = nullptr;
+  void (*proxy_abi_version_0_2_0_)() = nullptr;
   void (*proxy_on_log_)(uint32_t context_id) = nullptr;
   uint32_t (*proxy_validate_configuration_)(uint32_t root_context_id,
                                             uint32_t plugin_configuration_size) = nullptr;

include/proxy-wasm/null_vm.h
@@ -36,6 +36,7 @@ struct NullVm : public WasmVm {
   Cloneable cloneable() override { return Cloneable::InstantiatedModule; };
   std::unique_ptr<WasmVm> clone() override;
   bool load(const std::string &code, bool allow_precompiled) override;
+  AbiVersion getAbiVersion() override;
   bool link(string_view debug_name) override;
   uint64_t getMemorySize() override;
   optional<string_view> getMemory(uint64_t pointer, uint64_t size) override;

include/proxy-wasm/wasm.h
@@ -122,6 +122,8 @@ public:
   virtual void error(string_view message) { std::cerr << message << "\n"; }
   virtual void unimplemented() { error("unimplemented proxy-wasm API"); }
 
+  AbiVersion abiVersion() { return abi_version_; }
+
   bool getEmscriptenVersion(uint32_t *emscripten_metadata_major_version,
                             uint32_t *emscripten_metadata_minor_version,
                             uint32_t *emscripten_abi_major_version,
@@ -184,8 +186,6 @@ protected:
   std::unique_ptr<ShutdownHandle> shutdown_handle_;
   std::unordered_set<ContextBase *> pending_done_; // Root contexts not done during shutdown.
 
-  WasmCallVoid<0> abi_version_0_1_0_;
-
   WasmCallVoid<0> _start_; /* Emscripten v1.39.0+ */
   WasmCallVoid<0> __wasm_call_ctors_;
 
@@ -205,12 +205,14 @@ protected:
   WasmCallVoid<2> on_downstream_connection_close_;
   WasmCallVoid<2> on_upstream_connection_close_;
 
-  WasmCallWord<3> on_request_headers_;
+  WasmCallWord<2> on_request_headers_abi_01_;
+  WasmCallWord<3> on_request_headers_abi_02_;
   WasmCallWord<3> on_request_body_;
   WasmCallWord<2> on_request_trailers_;
   WasmCallWord<2> on_request_metadata_;
 
-  WasmCallWord<3> on_response_headers_;
+  WasmCallWord<2> on_response_headers_abi_01_;
+  WasmCallWord<3> on_response_headers_abi_02_;
   WasmCallWord<3> on_response_body_;
   WasmCallWord<2> on_response_trailers_;
   WasmCallWord<2> on_response_metadata_;
@@ -237,6 +239,9 @@ protected:
   std::string vm_configuration_;
   bool allow_precompiled_ = false;
   bool failed_ = false; // The Wasm VM has experienced a fatal error.
+
+  // ABI version.
+  AbiVersion abi_version_ = AbiVersion::Unknown;
 
   bool is_emscripten_ = false;
   uint32_t emscripten_metadata_major_version_ = 0;

include/proxy-wasm/wasm_api_impl.h
@@ -25,6 +25,13 @@ namespace null_plugin {
 
 inline WasmResult wordToWasmResult(Word w) { return static_cast<WasmResult>(w.u64_); }
 
+// Configuration and Status
+inline WasmResult proxy_get_configuration(const char **configuration_ptr,
+                                          size_t *configuration_size) {
+  return wordToWasmResult(
+      exports::get_configuration(current_context_, WR(configuration_ptr), WR(configuration_size)));
+}
+
 inline WasmResult proxy_get_status(uint32_t *code_ptr, const char **ptr, size_t *size) {
   return wordToWasmResult(exports::get_status(current_context_, WR(code_ptr), WR(ptr), WR(size)));
 }
@@ -57,6 +64,12 @@ inline WasmResult proxy_set_property(const char *key_ptr, size_t key_size, const
 }
 
 // Continue
+inline WasmResult proxy_continue_request() {
+  return wordToWasmResult(exports::continue_request(current_context_));
+}
+inline WasmResult proxy_continue_response() {
+  return wordToWasmResult(exports::continue_response(current_context_));
+}
 inline WasmResult proxy_continue_stream(WasmStreamType stream_type) {
   return wordToWasmResult(exports::continue_stream(current_context_, WS(stream_type)));
 }
@@ -73,6 +86,10 @@ proxy_send_local_response(uint32_t response_code, const char *response_code_deta
       WS(response_code_details_size), WR(body_ptr), WS(body_size),
       WR(additional_response_header_pairs_ptr), WS(additional_response_header_pairs_size),
       WS(grpc_status)));
+}
+
+inline WasmResult proxy_clear_route_cache() {
+  return wordToWasmResult(exports::clear_route_cache(current_context_));
 }
 
 // SharedData

commit 7fd61645e359fad302f742da640add74b7cfe1af

Fix C++14 compatibility.
Broken in #45.

Signed-off-by: Piotr Sikora <piotrsikora@google.com>

src/v8/v8.cc
@@ -373,7 +373,7 @@ AbiVersion V8::getAbiVersion() {
   const auto export_types = module_.get()->exports();
   for (size_t i = 0; i < export_types.size(); i++) {
     if (export_types[i]->type()->kind() == wasm::EXTERN_FUNC) {
-      std::string_view name(export_types[i]->name().get(), export_types[i]->name().size());
+      string_view name(export_types[i]->name().get(), export_types[i]->name().size());
       if (name == "proxy_abi_version_0_1_0") {
         return AbiVersion::ProxyWasm_0_1_0;
       } else if (name == "proxy_abi_version_0_2_0") {