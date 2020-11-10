fa1c9c442fc92bd93b4dbd8674da60e1bfda4e7f
Piotr Sikora <piotrsikora@google.com>
2020-11-10 07:54:38 +0000

    review: StopIteration across Contexts.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- include/proxy-wasm/context.h
+++ include/proxy-wasm/context.h
@@ -167,11 +167,6 @@ public:
   // Called before deleting the context.
   virtual void destroy();
 
-  // Called to raise the flag which indicates that the context should stop iteration regardless of
-  // returned filter status from Proxy-Wasm extensions. For example, we ignore
-  // FilterHeadersStatus::Continue after a local reponse is sent by the host.
-  void stopIteration() { stop_iteration_ = true; };
-
   /**
    * Calls into the VM.
    * These are implemented by the proxy-independent host code. They are virtual to support some
@@ -391,7 +386,6 @@ protected:
   std::shared_ptr<PluginBase> plugin_;
   bool in_vm_context_created_ = false;
   bool destroyed_ = false;
-  bool stop_iteration_ = false;
 
 private:
   // helper functions
