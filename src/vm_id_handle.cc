cf9d540b80c05146fc5e61ca09f97839a0065156
mathetake <takeshi@tetrate.io>
2020-12-09 08:23:00 +0900

    Review: fix initialization fiasco for vm_id_handle
    Signed-off-by: mathetake <takeshi@tetrate.io>

--- src/vm_id_handle.cc
+++ src/vm_id_handle.cc
@@ -23,38 +23,49 @@
 
 namespace proxy_wasm {
 
-std::unordered_map<std::string, std::weak_ptr<VmIdHandle>> global_vm_id_handles;
-std::vector<std::function<void(std::string_view vm_id)>> global_vm_id_handle_callbacks;
+std::unordered_map<std::string, std::weak_ptr<VmIdHandle>> *global_vm_id_handles = nullptr;
+std::vector<std::function<void(std::string_view vm_id)>> *global_vm_id_handle_callbacks = nullptr;
 std::mutex global_vm_id_handle_mutex;
 
 std::shared_ptr<VmIdHandle> getVmIdHandle(std::string_view vm_id) {
   std::lock_guard<std::mutex> lock(global_vm_id_handle_mutex);
+  if (!global_vm_id_handle_callbacks) {
+    global_vm_id_handle_callbacks = new std::vector<std::function<void(std::string_view vm_id)>>;
+    global_vm_id_handles = new std::unordered_map<std::string, std::weak_ptr<VmIdHandle>>;
+  }
+
   auto key = std::string(vm_id);
-  auto it = global_vm_id_handles.find(key);
-  if (it != global_vm_id_handles.end()) {
+  auto it = global_vm_id_handles->find(key);
+  if (it != global_vm_id_handles->end()) {
     auto handle = it->second.lock();
     if (handle) {
       return handle;
     }
-    global_vm_id_handles.erase(key);
+    global_vm_id_handles->erase(key);
   }
 
   auto handle = std::make_shared<VmIdHandle>(key);
-  global_vm_id_handles[key] = handle;
+  (*global_vm_id_handles)[key] = handle;
   return handle;
 };
 
 void registerVmIdHandleCallback(std::function<void(std::string_view vm_id)> f) {
   std::lock_guard<std::mutex> lock(global_vm_id_handle_mutex);
-  global_vm_id_handle_callbacks.push_back(f);
+  if (!global_vm_id_handle_callbacks) {
+    global_vm_id_handle_callbacks = new std::vector<std::function<void(std::string_view vm_id)>>;
+    global_vm_id_handles = new std::unordered_map<std::string, std::weak_ptr<VmIdHandle>>;
+  }
+  global_vm_id_handle_callbacks->push_back(f);
 }
 
 VmIdHandle::~VmIdHandle() {
   std::lock_guard<std::mutex> lock(global_vm_id_handle_mutex);
-  for (auto f : global_vm_id_handle_callbacks) {
-    f(vm_id_);
+  if (global_vm_id_handle_callbacks) {
+    for (auto f : *global_vm_id_handle_callbacks) {
+      f(vm_id_);
+    }
+    global_vm_id_handles->erase(vm_id_);
   }
-  global_vm_id_handles.erase(vm_id_);
 }
 
 } // namespace proxy_wasm
