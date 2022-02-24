21976e44118d815c155855f85f9a1818ec0f7941
Piotr Sikora <piotrsikora@google.com>
2022-02-24 03:03:31 -0600

    build: add support for Clang with Thread Sanitizer. (#266)
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- test/runtime_test.cc
+++ test/runtime_test.cc
@@ -128,7 +128,14 @@ TEST_P(TestVM, CloneUntilOutOfMemory) {
     // Prevent clone from droping out of scope and freeing memory.
     clones.push_back(std::move(clone));
   }
-  EXPECT_GE(clones.size(), 1000);
+
+  size_t min_clones = 1000;
+#if defined(__has_feature)
+#if __has_feature(thread_sanitizer)
+  min_clones = 100;
+#endif
+#endif
+  EXPECT_GE(clones.size(), min_clones);
 }
 
 #endif
