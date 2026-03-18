04b391b381417b96e2b1f01a276a14db61ae2352
Matt Leon <mattleon@google.com>
2026-03-18 17:28:30 -0400

    Remove unused import from arg_passing.rs
    Signed-off-by: Matt Leon <mattleon@google.com>

--- test/test_data/arg_passing.rs
+++ test/test_data/arg_passing.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::time::{SystemTime, UNIX_EPOCH};
-
 extern "C" {
     fn proxy_log(level: u32, message_data: *const u8, message_size: usize) -> bool;
 }
